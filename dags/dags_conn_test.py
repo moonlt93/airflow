0a3625c1b24212b020c4d04025e1f235e99ad506
jinsu <moonlt93@github.com>
2025-08-15 12:10:38 +0900

    refacotr: task 이름 수정

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -4,7 +4,7 @@ import pendulum
 from airflow.providers.standard.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="dags_bash_operator", # 화면 이름 , python 파일명과 관련없음. -> 근데 일치 시키는게 좋음 몇백개 이상임.
+    dag_id="dags_conn_test", # 화면 이름 , python 파일명과 관련없음. -> 근데 일치 시키는게 좋음 몇백개 이상임.
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2025, 8, 15, tz="Asia/Seoul"),
     catchup=False, # 누락 구간을 돌릴지 결정 -> bulk 성 데이터를 한꺼번에 돌림.. 
